0301b0920e93a57103459665b6d783e0a70ab14f
sdove84 <sdove29.84@gmail.com>
2016-10-24 16:33:45 -0700

    calculator-surprise

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /**
  * Created by seandove on 10/12/16.
  */
+//declare variable val_array
 var val_array = {
     value: '',
     type: null
@@ -24,12 +25,17 @@ var global_array = [];
 //var my_calculator = new calculator(callback);
 //after DOM load add click handlers to all buttons
 $(document).ready(function () {
+    //  LFZ START
+    //a function of on click is called for button
     $('button').on('click', function () {
+        //sets the variable val and calls this and returns
         var val = $(this).text();
+        //switch statement compares the value of each case
         switch (val) {
             /* case 'CE':
              allClear();
              break;*/
+            //called when values don't match expression
             default:
                 input_number(val);
                 input_type(val);
@@ -38,16 +44,23 @@ $(document).ready(function () {
                 break;
         }
     });
-})
-
+    // LFZ FINISH
+});
+//sets function for input_number with a peram of value
 function input_number(value) {
+    //concatinates
     val_array.value += value;
+    //consoles out result of concatinatoin
     console.log('value is', val_array);
 }
+//sets function to input type with a peram of button
 function input_type(button) {
     //if
+    //  LFZ START
+    //switch statment to call button peram to compare value
     switch (button) {
         //if button === +
+            //case returns the variable to switch statement
         case '/':
             //do this
             val_array.type = 'operator';
@@ -68,4 +81,5 @@ function input_type(button) {
         default:
             val_array.type = 'numbers';
     }
+    //  LFZ FINISH
 }
