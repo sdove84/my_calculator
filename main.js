47ee6ccb15f3198ed22c278c61a60aeba83eb74b
sdove84 <sdove29.84@gmail.com>
2017-05-16 02:00:43 -0700

    working version

--- main.js
+++ main.js
@@ -1,85 +1,98 @@
 /**
  * Created by seandove on 10/12/16.
  */
-//declare variable val_array
-var val_array = {
-    value: '',
-    type: null
-};
-var global_array = [];
 
-
-//callback function defined
-//function callback(type, value, item) {
-/* switch (value) {
- case undefined:
- $('#display_area').html("");
- break;
- default:
- $('#display_area').html(value);
- break;
-
- }
- }*/
-// my_calculator - creates a new calculator object
-//var my_calculator = new calculator(callback);
-//after DOM load add click handlers to all buttons
 $(document).ready(function () {
-    //  LFZ START
-    //a function of on click is called for button
+    var input_array=[''];
+    var input_index=0;
+
     $('button').on('click', function () {
-        //sets the variable val and calls this and returns
         var val = $(this).text();
-        //switch statement compares the value of each case
-        switch (val) {
-            /* case 'CE':
-             allClear();
-             break;*/
-            //called when values don't match expression
-            default:
-                input_number(val);
-                input_type(val);
-                /*case 'C':
-                 clear();*/
-                break;
-        }
     });
-    // LFZ FINISH
-});
-//sets function for input_number with a peram of value
-function input_number(value) {
-    //concatinates
-    val_array.value += value;
-    //consoles out result of concatinatoin
-    console.log('value is', val_array);
-}
-//sets function to input type with a peram of button
-function input_type(button) {
-    //if
-    //  LFZ START
-    //switch statment to call button peram to compare value
-    switch (button) {
-        //if button === +
-            //case returns the variable to switch statement
-        case '/':
-            //do this
-            val_array.type = 'operator';
-            break;
-        //else if
-        case 'x':
-            val_array.type = 'operator';
-            break;
-        case '-':
-            val_array.type = 'operator';
-            break;
-        case '+':
-            val_array.type = 'operator';
-            break;
-        case '=':
-            val_array.type = 'equalSign';
-            break;
-        default:
-            val_array.type = 'numbers';
+    apply_click_handlers();
+
+    function apply_click_handlers(){
+        $('.number').click(input_number);
+        $('.operator').click(input_operator);
+        $('#equal_button').click(calculate);
+        $('.clear').click(clear);
     }
-    //  LFZ FINISH
-}
+
+    function input_number(){
+        console.log( $(this).text());
+        input_array[input_index] += $(this).text();
+        $("#display_area").empty();
+        $("#display_area").append(input_array);
+
+        console.log(input_array);
+    }
+
+    function input_operator(){
+        //do I have enough to do math?
+        //as long as do math returns it to the original state
+        if (input_array.length===3){
+            calculate();
+        }
+        input_index++;
+        input_array[input_index] = $(this).text();
+        console.log(input_array);
+        input_index++;
+        input_array[input_index] = '';
+    }
+
+    function calculate(){
+        var operator=input_array[1];
+        var num1=parseFloat(input_array[0]);
+        var num2=parseFloat(input_array[2]);
+        var total=0;
+
+        switch (operator){
+            case "+":
+                total=num1+num2;
+                input_array=[''];
+                input_array[0]=total;
+                $("#display_area").empty();
+                $("#display_area").append(total);
+                input_index=0;
+                console.log(total);
+                break;
+            case "-":
+                total=num1-num2;
+                input_array=[''];
+                input_array[0]=total;
+                $("#display_area").empty();
+                $("#display_area").append(total);
+                input_index=0;
+                console.log(total);
+                break;
+            case "x":
+                total=num1*num2;
+                input_array=[''];
+                input_array[0]=total;
+                $("#display_area").empty();
+                $("#display_area").append(total);
+                input_index=0;
+                console.log(total);
+                break;
+            case "/":
+                total=num1/num2;
+                input_array=[''];
+                input_array[0]=total;
+                $("#display_area").empty();
+                $("#display_area").append(total);
+                input_index=0;
+                console.log(total);
+                break;
+            default:
+                total=0;
+        }
+    }
+
+    function clear() {
+        $("#display_area").empty();
+        input_array=[''];
+        input_index=0;
+    }
+});
+
+
